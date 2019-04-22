1f331ca1fdd7b26234aecf66089f0f45104c102b
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-22 16:31:28 -0400

    refactor; introduce some abstraction

--- frame.rb
+++ frame.rb
@@ -17,12 +17,27 @@ class Frame
   def roll!(throws)
     # return strike if throws.size == 1
 
+    create_rolls(throws)
+    set_initial_score
+    toggle_spare
+    mark_played
+  end
+
+  def create_rolls(throws)
     throws.each do |throw|
       @rolls << Roll.create(pins_down: throw)
     end
+  end
 
+  def set_initial_score
     @score = @rolls.inject(0) { |sum, r| sum + r.pins_down }
+  end
+
+  def toggle_spare
     @spare = @score == MAX_NUMBER_OF_PINS
+  end
+
+  def mark_played
     @played = true
   end
 end
